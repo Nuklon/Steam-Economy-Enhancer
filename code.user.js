639b5bb97ef83abb206e9f951e403ecdcff0f86d
Nuklon <Nuklon@users.noreply.github.com>
2017-09-23 15:21:53 +0200

    6.0.1

--- code.user.js
+++ code.user.js
@@ -3,7 +3,7 @@
 // @namespace   https://github.com/Nuklon
 // @author      Nuklon
 // @license     MIT
-// @version     6.0.0
+// @version     6.0.1
 // @description Enhances the Steam Inventory and Steam Market.
 // @include     *://steamcommunity.com/id/*/inventory*
 // @include     *://steamcommunity.com/profiles/*/inventory*
@@ -1208,7 +1208,7 @@
 
         function sellSelectedItems() {
             getInventorySelectedMarketableItems(function (items) {
-                sellItems(filteredItems);
+                sellItems(items);
             });
         }
 
