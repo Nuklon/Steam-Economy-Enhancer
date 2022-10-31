93a6be34deac4f5b857be082391721f81eb9321b
Nuklon <Nuklon@users.noreply.github.com>
2022-10-31 14:25:14 +0100

    Add sell all duplicate items
    Closes #163 #160

--- code.user.js
+++ code.user.js
@@ -4,7 +4,7 @@
 // @namespace   https://github.com/Nuklon
 // @author      Nuklon
 // @license     MIT
-// @version     6.8.4
+// @version     6.8.5
 // @description Enhances the Steam Inventory and Steam Market.
 // @include     *://steamcommunity.com/id/*/inventory*
 // @include     *://steamcommunity.com/profiles/*/inventory*
@@ -1213,6 +1213,29 @@
                 });
         }
 
+        function sellAllDuplicateItems() {
+            loadAllInventories().then(function () {
+                var items = getInventoryItems();
+                var marketableItems = [];
+                var filteredItems = [];
+
+                items.forEach(function (item) {
+                    if (!item.marketable) {
+                        return;
+                    }
+
+                    marketableItems.push(item);
+                });
+
+                filteredItems = marketableItems.filter((e, i) => marketableItems.map(m => m.classid).indexOf(e.classid) !== i);
+
+                sellItems(filteredItems);
+            },
+            function () {
+                logDOM('Could not retrieve the inventory...');
+            });
+        }
+
         function sellAllCards() {
             loadAllInventories().then(function() {
                     var items = getInventoryItems();
@@ -2031,6 +2054,7 @@
 
             var sellButtons = $('<div id="inventory_sell_buttons" style="margin-bottom:12px;">' +
                 '<a class="btn_green_white_innerfade btn_medium_wide sell_all separator-btn-right"><span>Sell All Items</span></a>' +
+                '<a class="btn_green_white_innerfade btn_medium_wide sell_all_duplicates separator-btn-right"><span>Sell All Duplicate Items</span></a>' +
                 '<a class="btn_green_white_innerfade btn_medium_wide sell_selected separator-btn-right" style="display:none"><span>Sell Selected Items</span></a>' +
                 '<a class="btn_green_white_innerfade btn_medium_wide sell_manual separator-btn-right" style="display:none"><span>Sell Manually</span></a>' +
                 (showMiscOptions ?
@@ -2071,6 +2095,7 @@
                         sellAllItems(appId);
                     });
                 $('.sell_selected').on('click', '*', sellSelectedItems);
+                $('.sell_all_duplicates').on('click', '*', sellAllDuplicateItems);
                 $('.sell_manual').on('click', '*', sellSelectedItemsManually);
                 $('.sell_all_cards').on('click', '*', sellAllCards);
                 $('.sell_all_crates').on('click', '*', sellAllCrates);
