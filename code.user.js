0ff330eed3e3b77856db6c9ba844a3ed7fa1b227
Nuklon <Nuklon@users.noreply.github.com>
2023-12-22 12:31:23 +0100

    Update code.user.js

--- code.user.js
+++ code.user.js
@@ -4,7 +4,7 @@
 // @namespace   https://github.com/Nuklon
 // @author      Nuklon
 // @license     MIT
-// @version     6.9.0
+// @version     6.9.1
 // @description Enhances the Steam Inventory and Steam Market.
 // @include     *://steamcommunity.com/id/*/inventory*
 // @include     *://steamcommunity.com/profiles/*/inventory*
