2c7582680af184b6d25406b84455b8adb57a758a
Nuklon <Nuklon@users.noreply.github.com>
2025-12-01 22:47:56 +0100

    Bump version from 7.1.20 to 7.1.21

--- code.user.js
+++ code.user.js
@@ -4,7 +4,7 @@
 // @namespace    https://github.com/Nuklon
 // @author       Nuklon
 // @license      MIT
-// @version      7.1.20
+// @version      7.1.21
 // @description  Enhances the Steam Inventory and Steam Market.
 // @match        https://steamcommunity.com/id/*/inventory*
 // @match        https://steamcommunity.com/profiles/*/inventory*
